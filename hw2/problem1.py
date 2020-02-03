5cb6013c98bfc0c25030b5835e48e40b00982d9f
thetimeofblack <1191401005@qq.com>
2020-02-02 17:12:55 -0800

    hello world

---
+++ hw2/problem1.py
@@ -0,0 +1,61 @@
+import numpy as np
+from python3.plotDecBoundaries import plotDecBoundaries
+from tools.nearest_centroid_classifier import *
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import cdist
+import matplotlib.patches as mpatches
+
+def judgeClassOne(pixel):
+    return 0
+def judgeClassTwo(pixel):
+    return 0
+def judgeClassThree(pixel):
+    return 0
+
+'''
+train_data =np.array( [[1,2], [2,3]])
+train_labels = np.array([0,1])
+mean_sample = np.array([[0,0],[5,5]])
+plotDecBoundaries(train_data, train_labels, mean_sample)
+'''
+
+max_x = 10.0
+min_x =-10.0
+max_y = 10.0
+min_y = -10.0
+
+xrange = (min_x , max_x)
+yrange = (min_y ,max_y)
+
+inc = 0.05
+
+(x, y) = np.meshgrid(np.arange(xrange[0],xrange[1]+inc/100 ,inc),np.arange(yrange[0],yrange[1]+inc/100 ,inc))
+image_size = x.shape
+xy = np.hstack((x.reshape(x.shape[0]*x.shape[1],1,order='F'), y.reshape(y.shape[0]*y.shape[1] , 1, order='F')))
+
+
+pred_label = []
+for pixel in xy :
+    if (-pixel[0] - pixel[1] +5 >0) and (-pixel[0] +3 > 0) :
+        pred_label.append(1)
+    elif (-pixel[0] +pixel[1] - 1 > 0) and (-pixel[0]-pixel[1]+5 <0) :
+        pred_label.append(2)
+    elif  (-pixel[0] + 3 <0) and (-pixel[0] + pixel[1] -1<0)  :
+        pred_label.append(3)
+    else:
+        pred_label.append(0)
+
+pred_label = np.array(pred_label)
+decisionmap = pred_label.reshape(image_size,order='F')
+print(decisionmap)
+plt.legend(( '[4,1] Class 1' , '[1,5] Class 2' , '[0,0] Class 3') , loc = 3)
+plt.imshow(decisionmap, extent = [xrange[0],xrange[1],yrange[0],yrange[1]], origin = 'lower' )
+plt.plot(4,1 , 'rx')
+plt.plot(1,5 , 'go')
+plt.plot(0,0 , 'b*')
+plt.legend(( '[4,1] Class 1' , '[1,5] Class 2' , '[0,0] Class 3') , loc = 3)
+plt.gca().add_artist(l)
+yellow_patch = mpatches.Patch(color='yello',label = 'Class One Region')
+plt.legend(handles=[yellow_patch])
+
+plt.show()
